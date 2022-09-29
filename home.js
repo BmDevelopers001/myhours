ebcfc6000dc83d72fad0ee1071860e87584b67e4
BmDevelopers001 <bmsavaliya001@gmail.com>
2022-09-30 04:42:55 +0530

    day-2 work added

---
+++ home.js
@@ -0,0 +1,6 @@
+
+let imgDiv = document.querySelector(".business");
+
+let imgArr = ["https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/600008d9399ba39d2583a755_Capterra-4-7.png", "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/600008d9b4eb5ca178cc55df_g2-4-7.png", "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/600008d9487a040f81a5d142_MS-Gold.png"]
+
+console.log(imgArr[1])
