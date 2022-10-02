2bcb0230c6e96a2e9c851afa7595af1145da7bd0
BmDevelopers001 <bmsavaliya001@gmail.com>
2022-10-02 20:03:30 +0530

    day-6 work added

--- sign-in.js
+++ sign-in.js
@@ -10,10 +10,11 @@ function signedIn(){
     let y = localStorage.getItem("password");
     
     if(email.value == "" || pwd.value == ""){
-        alert("Please fill necessary details")
+        alert("Please fill necessary details");
     } else if(email.value == x && pwd.value == y){
-        alert("Logged in successfully");
+        // alert("Logged in successfully");
+        signIn.href = "profile.html";
     } else{
-        alert("Invalid Login credentials")
+        alert("Invalid Login credentials");
     }
 }
